05195008b9cebf02fdee6ad194ec01c68e5c73b0
Edsel Barbosa González <edselbarbosagonzalez@gmail.com>
2018-05-13 11:23:19 -0500

    Carga inicial

---
+++ Lector Codigos QR/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Lector_Codigos_QR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnTerminar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(356, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Lector codigo QR";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(342, 35);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(248, 212);
+            this.listBox1.TabIndex = 10;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(14, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(152, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(181, 5);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 7;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnTerminar
+            // 
+            this.btnTerminar.Location = new System.Drawing.Point(262, 6);
+            this.btnTerminar.Name = "btnTerminar";
+            this.btnTerminar.Size = new System.Drawing.Size(75, 23);
+            this.btnTerminar.TabIndex = 9;
+            this.btnTerminar.Text = "Finalizar";
+            this.btnTerminar.UseVisualStyleBackColor = true;
+            this.btnTerminar.Click += new System.EventHandler(this.btnTerminar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.Controls.Add(this.videoSourcePlayer1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.listBox1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.btnIniciar);
+            this.panel1.Controls.Add(this.btnTerminar);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 260);
+            this.panel1.TabIndex = 22;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(15, 33);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(322, 217);
+            this.videoSourcePlayer1.TabIndex = 23;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 278);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnTerminar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+    }
+}
+
---
+++ Lector Codigos QR/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using BarcodeLib.BarcodeReader;
+using System.IO;
+
+namespace Lector_Codigos_QR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        FilterInfoCollection dispositivos;
+        VideoCaptureDevice fuentevideo;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo x in dispositivos)
+            {
+                comboBox1.Items.Add(x.Name);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer1.Start();
+            fuentevideo = new VideoCaptureDevice(dispositivos[comboBox1.SelectedIndex].MonikerString);
+            videoSourcePlayer1.VideoSource = fuentevideo;
+            videoSourcePlayer1.Start();
+        }
+
+        private void btnTerminar_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            videoSourcePlayer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (videoSourcePlayer1.GetCurrentVideoFrame() != null)
+            {
+                Bitmap img = new Bitmap(videoSourcePlayer1.GetCurrentVideoFrame());
+                string[] resultados = BarcodeReader.read(img, BarcodeReader.QRCODE);
+                img.Dispose();
+                if ((resultados != null) && (resultados.Length > 0))
+                    listBox1.Items.Add(resultados[0]);
+            }
+        }
+    }
+}
